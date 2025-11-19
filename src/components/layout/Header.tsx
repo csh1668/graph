818697d4cbf557535f1c42f9ab7a8e97e5e41f78
SeoHyeon <tjgus1668@naver.com>
2025-11-19 23:48:00 +0900

    chore: modify Header design

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,8 @@ import { Github } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="relative z-10 bg-background/50 backdrop-blur-sm px-6 py-2 flex items-center justify-between border-b">
-      <h1 className="text-xl font-bold">Graph Visualizer</h1>
+    <header className="relative z-10 bg-background/50 backdrop-blur-sm px-8 py-4 flex items-center justify-between border-b">
+      <h1 className="text-2xl font-bold">Graph Visualizer</h1>
       <nav className="flex items-center gap-2">
         <a href="https://github.com/csh1668/graph" target="_blank" rel="noreferrer">
           <Github className="h-5 w-5" />
